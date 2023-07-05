f6d4c8f2cd9b5beaae3ef2f720a004a3319632c9
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-05 15:53:55 -0400

    http broke (node modules err)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,20 +3,28 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 }) // @decorator means metadata
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService,
+    private http: HttpClient) { }
   /* serivce-in-service scenario: inject MessageService
   into HeroService which is injected into HeroesComponent */
+
+  private log(message: string) { // frequent calls so use log()
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = "api/heroes";
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES); // of returns an Observable<Hero> that emits the mock heroes array
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
